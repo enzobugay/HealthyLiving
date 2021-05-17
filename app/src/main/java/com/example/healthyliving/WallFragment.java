5dad03e5b56ed723e4d143c00ba92a3d09407ac8
Mary Licudo <licudomary@gmail.com>
2021-05-17 11:29:02 +0800

    Added getInput method in Wall Fragment Activity

--- app/src/main/java/com/example/healthyliving/WallFragment.java
+++ app/src/main/java/com/example/healthyliving/WallFragment.java
@@ -2,20 +2,22 @@ package com.example.healthyliving;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ListView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,15 +25,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
 public class WallFragment extends Fragment {
 
     EditText inputwall;
     FirebaseDatabase firebaseDatabase;
-    ProgressDialog progressDialog;
 
     String inputwallB, anonymousB = "false";
     int count = 0;
     int entries;
+    int childCount;
 
 
     @Nullable
@@ -62,9 +68,24 @@ public class WallFragment extends Fragment {
                 }
             }
         });
+
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button viewPost = (Button) view.findViewById(R.id.viewPost);
+        viewPost.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                getInput();
+            }
+        }
+        );
+    }
 
     //----------------------------Send wall message to DB method----------------------------------
     public void postInput(){
@@ -102,19 +123,54 @@ public class WallFragment extends Fragment {
                 databaseReference.child(String.valueOf(childentries)).child("name").setValue("Jose Bugay");
             }
         }
-    }
 
+    }
+    //display wall message from db
     public void getInput(){
+        
+        //Static display of list
+        /**
+        ListView listView = getView().findViewById(R.id.listView);
+        String[] list = {"This is a sample post", "Another post","Last Post","This is a test"};
+        ArrayAdapter adapter = new ArrayAdapter<String> (getActivity() , R.layout.list_item, list);
+        listView.setAdapter(adapter);
+         **/
 
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage("Loading...");
-        progressDialog.setCancelable(false);
-        progressDialog.setCanceledOnTouchOutside(false);
-        progressDialog.show();
-
+        /**
+         * Dynamic display of list
+         * */
+        ListView listView = getView().findViewById(R.id.listView);
+        ArrayList <String> list = new ArrayList<String>();
+        ArrayAdapter adapter = new ArrayAdapter<String> (getActivity(), R.layout.list_item,list);
+        listView.setAdapter(adapter);
         firebaseDatabase = FirebaseDatabase.getInstance("https://healthyliving-da296-default-rtdb.asia-southeast1.firebasedatabase.app/");
-        DatabaseReference databaseReference = firebaseDatabase.getReference("/wall");
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("wall");
 
+        ValueEventListener valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    //getting specific child branch "input"  to add in the list
+                    list.add(ds.child("input").getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+                //System.out.println(list);
+            }
+
+            //Single node display of text
+
+            /**DatabaseReference databaseReference = firebaseDatabase.getReference("wall").child("1");
+             viewPost = getView().findViewById(R.id.viewWallPost);
+             String post = snapshot.child("input").getValue().toString();
+             viewPost.setText(post);
+             ***/
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
 
     }
+
 }
